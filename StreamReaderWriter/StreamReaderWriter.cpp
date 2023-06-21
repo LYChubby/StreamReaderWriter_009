a4eebb5b57dd723484d3d439b7449c482f39a33f
LYChubby <f4uzi345@gmail.com>
2023-06-21 14:52:02 +0700

    Membuat Unlimited Loop Untuk Menulis

--- StreamReaderWriter/StreamReaderWriter.cpp
+++ StreamReaderWriter/StreamReaderWriter.cpp
@@ -12,4 +12,15 @@ int main() {
 	outfile.open("ContohFile.txt");
 
 	cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+
+	// Unlimited Loop Untuk Menulis
+	while (true) {
+		cout << "- ";
+		// Mendapatkan Setiap Karakter Dalam Satu Baris
+		getline(cin, baris);
+		// Loop Akan Berhenti Jika Anda Memasukkan Karakter q
+		if (baris == "q") break;
+		// Menilis Dan Memasukkan Nilai Dari "Baris" Ke Dalam File
+		outfile << baris << endl;
+	}
 }
